4e9c589a519a6f6c703abcce177e974665c8c8f5
Vladislav Yarmak <yarmak.vladislav@gmail.com>
2016-04-04 15:06:44 +0300

    stateful parser

--- PrintingReader.scala
+++ PrintingReader.scala
@@ -1,44 +1,99 @@
 import org.scalastuff.json.JsonHandler
 import scala.collection.mutable.Stack
 
+object ContainerType extends Enumeration {
+      type ContainerType = Value
+      val ObjectContainer, ArrayContainer, KeyContainer = Value
+    }
+import ContainerType._
+
 class PrintingReader(printer: String => Unit) extends org.scalastuff.json.JsonHandler {
-  var paths = Stack[String]()
-  var arrayIndex: Long = 0
-  var currentKey = "<root>"
+  var ctx = Stack[Tuple3[ContainerType,Long,String]]()
+  var currentKey = ""
+
+  def printStack() = {
+    printer(ctx.toList.toString)
+  }
+  
+  def getPath() = {
+    var paths = Stack[String]()
+    for (c <- ctx) {
+      val p = c match {
+        case (ObjectContainer, _, path: String) => path
+        case (ArrayContainer, idx: Long, _) => (idx-1).toString
+      }
+      paths.push(p)
+    }
+    paths.mkString(".")
+  }
+
+  def printElem(e: String) = {
+    printer(s"${getPath} => ${e}")
+  }
+
+  def incStack() = {
+    if (!ctx.isEmpty) {
+      val (t, i, k) = ctx.pop
+      val ni = t match {
+        case ArrayContainer => i + 1
+        case ObjectContainer => i
+        case _ => { printer("ERROR"); System.exit(5); i; }
+      }
+      ctx.push((t,ni,k))
+    }
+  }
 
   def startObject() = {
     printer("startObject")
-    paths.push(currentKey)
+    incStack
+    ctx.push((ObjectContainer, 0, ""))
   }
+
   def startMember(name: String) = {
-    currentKey = name
     printer(s"startMember '${name}'")
+    val (t, i, k) = ctx.pop
+    ctx.push((t,i,name))
   }
+
   def endObject() = {
     printer("endObject")
+    ctx.pop
   }
 
   def startArray() = {
     printer("startArray")
+    incStack
+    ctx.push((ArrayContainer, 0, ""))
   }
+
   def endArray() = {
     printer("endArray")
+    ctx.pop
   }
 
   def string(s: String) = {
-    printer(s"string = '${s}'")
+    incStack
+    printElem(s"string = '${s}'")
   }
+
   def number(n: String) = {
-    printer(s"number = ${n}")
+    incStack
+    printElem(s"number = ${n}")
   }
+
   def trueValue() = {
-    printer("true")
+    incStack
+    printElem("true")
   }
+
   def falseValue() = {
-    printer("false")
+    incStack
+    printElem("false")
   }
+
   def nullValue() = {
-    printer("null")
+    incStack
+    printElem("null")
   }
 
   def error(message: String, line: Int, pos: Int, excerpt: String) {
